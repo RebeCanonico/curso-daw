7ff0a70427cfee16284a6ac07cedc8c9bfe77666
RebeCanonico <rebeca.27.cc@gmail.com>
2022-12-11 20:46:50 +0100

    .

--- Frontend/JavaScript/Ejercicios-nov/contador-de-palabras.js
+++ Frontend/JavaScript/Ejercicios-nov/contador-de-palabras.js
@@ -5,10 +5,14 @@ let wordCount = document.getElementById("word-count");
 
 inputTextArea.addEventListener("input", () => {
     // en la constante guardamos el numero de letras
-    characCount.textContent = inputTextArea.value.length;
+    let noContarEspacios = inputTextArea.value;
     // console.log(characCount.textContent, 'hola')
     // le decimos que a la constante inputTextArea le guarde el numero de letras pero sin espacios. con trim().
-    let txt = inputTextArea.value.trim();
+    // let txt = inputTextArea.value.trim();
+    characCount.textContent = noContarEspacios.replace( /\s/g, '' ).length
     // la constante wordCount utiliza la varia txt y slipt lo combierte en un array y con filter filtro cada palabra y la suma
-    wordCount.textContent = txt.split(/\s+/).filter((item) => item).length;
+    // wordCount.textContent = txt.split(/\s+/).filter((item) => item).length;
+    wordCount.textContent = inputTextArea.value.split(/\s+/).filter(
+        (item) => item
+        ).length;
 });
