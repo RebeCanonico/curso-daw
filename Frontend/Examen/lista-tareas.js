f48d9c84f157c9a8b5c80355413213213021cb38
RebeCanonico <rebeca.27.cc@gmail.com>
2022-12-21 19:40:52 +0100

    21/12/2022

--- Frontend/Examen/lista-tareas.js
+++ Frontend/Examen/lista-tareas.js
@@ -1,6 +1,6 @@
 
 // AQUI INICIA LA EJECUCION CUANDO LLEGO A LA PAGINA
-// Create a "close" button and append it to each list item
+// crear boton cerrar y añadirselo a cada elemento
 let miListaTareas = document.getElementsByTagName("li");
 let j;
 for (j = 0; j < miListaTareas.length; j++) {
@@ -10,6 +10,14 @@ for (j = 0; j < miListaTareas.length; j++) {
   span.appendChild(txt);
   miListaTareas[j].appendChild(span);
 }
+
+// Add a "checked" symbol when clicking on a list item
+let list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
   
 
 // se ejecuta la primera vez cuando carga la pagina para poner el onclick a la X de cerrar
@@ -23,24 +31,21 @@ function cargarTareas(){
   for (i = 0; i < cruces.length; i++) {
       cruces[i].onclick = function() {
           let listItem = this.parentElement;
+          listItem.classList.remove('d-flex');
           listItem.style.display = "none";
       }
   }
-  // Add a "checked" symbol when clicking on a list item
-  let list = document.querySelector('ul');
-  list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-      ev.target.classList.toggle('checked');
-    }
-  }, false);
 }
 // Crear un nuevo elemento de lista al hacer clic en el botón "Agregar"
 function nuevaTarea() {
     
     let li = document.createElement("li");
     let inputValue = document.getElementById("tarea").value;
+    let spanTexto = document.createElement("SPAN");
     let texto = document.createTextNode(inputValue);
-    li.appendChild(texto);
+    spanTexto.classList.add('mx-4')
+    spanTexto.appendChild(texto);
+    li.appendChild(spanTexto);
     if (inputValue === '') {
       alert("¡Tienes que escribir algo!");
     } else {
@@ -48,10 +53,14 @@ function nuevaTarea() {
     }
     document.getElementById("tarea").value = "";
     let span = document.createElement("SPAN");
-    let txt = document.createTextNode("\u00D7");
+    let icon = document.createElement('img');
+    icon.src = 'eliminar.png';
+    icon.classList.add('img-fluid');
+    // let txt = document.createTextNode("\u00D7");
     span.className = "eliminar";
-    span.appendChild(txt);
+    span.appendChild(icon);
     li.appendChild(span);
+    li.classList.add('d-flex', 'justify-content-between', 'align-items-center', "my-1");
 
     cargarTareas();
 }
