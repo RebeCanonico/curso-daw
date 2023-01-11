8aad18c65d7e0077eacca0264709cf651f258aa5
RebeCanonico <rebeca.27.cc@gmail.com>
2023-01-11 14:04:55 +0100

    clase 11.01.23

--- Backend/emp_dep.sql
+++ Backend/emp_dep.sql
@@ -49,7 +49,7 @@ SELECT nomEmp AS Nombre, cargoE AS Cargo
 FROM empleados;
 
 -- 14. Hallar el salario y la comisión de aquellos empleados cuyo número de documento de identidad es superior al ‘19.709.802’
-SELECT nDIEmp, salEmp, comisionE 
+SELECT , salEmp, comisionE 
 FROM empleados
 WHERE nDIEmp > '19.709.802';
 
@@ -58,3 +58,96 @@ SELECT nomEmp, cargoE
 FROM empleados
 WHERE lower(nomEmp) > 'j' and lower(nomEmp) < 'z'
 ORDER BY nomEmp;
+
+-- 16. Listar el salario, la comisión, el salario total (salario + comisión), documento de identidad del empleado y nombre, de aquellos empleados que tienen comisión superior a 1.000.000, ordenar el informe por el número del documento de identidad
+SELECT salemp, comisione, (salemp + comisione) AS 'Salario total', ndiemp, nomemp
+FROM empleados
+WHERE comisione > 1000000
+ORDER BY ndiemp;
+
+-- 17. Obtener un listado similar al anterior, pero de aquellos empleados que NO tienen comisión
+SELECT salemp, comisione, (salemp + comisione) AS 'Salario total', ndiemp, nomemp
+FROM empleados
+WHERE comisione = 0;
+
+-- 18. Hallar los empleados cuyo nombre no contiene la cadena «MA»
+SELECT nomemp
+FROM empleados
+WHERE lower(nomEmp) not like '%ma%';
+
+-- 19. Obtener los nombres de los departamentos que no sean “Ventas” ni “Investigación” NI ‘MANTENIMIENTO’
+SELECT nombredpto
+FROM departamentos
+WHERE lower(nombredpto) NOT IN ('ventas', 'investigación', 'mantenimiento');
+
+-- 20. Obtener el nombre y el departamento de los empleados con cargo ‘Secretaria’ o ‘Vendedor’, que no trabajan en el departamento de “PRODUCCION”, cuyo salario es superior a $1.000.000, ordenados por fecha de incorporación
+SELECT e.nomemp, d.nombredpto, cargoe
+FROM empleados e, departamentos d
+WHERE e.coddepto = d.coddepto AND (LOWER(e.cargoe)='secretaria' OR LOWER(e.cargoe)='vendedor')
+AND LOWER(d.nombredpto) NOT IN ('producción') AND e.salemp > 1000000
+ORDER BY e.fecincorporacion;
+
+-- 21. Obtener información de los empleados cuyo nombre tiene exactamente 11 caracteres
+SELECT *
+FROM empleados
+WHERE char_length(nomemp) = 11;
+
+-- 22. Obtener información de los empleados cuyo nombre tiene al menos 11 caracteres
+SELECT *
+FROM empleados
+WHERE char_length(nomemp) < 11;
+
+-- 23. Listar los datos de los empleados cuyo nombre inicia por la letra ‘M’, su salario es mayor a $800.000 o reciben comisión y trabajan para el departamento de ‘VENTAS’
+SELECT e.*
+FROM empleados e, departamentos d
+WHERE e.coddepto = d.coddepto AND LOWER(e.nomemp) LIKE 'm%' AND (e.salemp > 800000 OR e.comisione > 0)
+AND LOWER(d.nombredpto) = 'ventas';
+
+-- 24. Obtener los nombres, salarios y comisiones de los empleados que reciben un salario situado entre la mitad de la comisión la propia comisión
+SELECT nomemp, salemp, comisione
+FROM empleados
+WHERE salemp BETWEEN (comisione/2) AND comisione;
+
+-- 25. Mostrar el salario más alto de la empresa
+SELECT nomEmp, salEmp
+FROM empleados
+ORDER BY salEmp DESC
+LIMIT 1;
+
+-- 26. Mostrar cada una de las comisiones y el número de empleados que las reciben. Solo si tiene comisión
+SELECT comisionE, COUNT(*) AS 'Numero empleados'
+FROM empleados GROUP BY comisionE HAVING comisionE > 0;
+
+-- 27. Mostrar el nombre del último empleado de la lista por orden alfabético
+SELECT MAX(nomemp) AS 'Mayor alfabeticamente'
+FROM empleados;
+
+-- 28. Hallar el salario más alto, el más bajo y la diferencia entre ellos
+SELECT MAX(salEmp) AS 'Salario mayor', MIN(salEmp) AS 'Salario menor', MAX(salEmp) - MIN(salEmp) AS 'Diferencia'
+FROM empleados;
+
+-- 29. Mostrar el número de empleados de sexo femenino y de sexo masculino, por departamento
+SELECT codDepto, sexemp, COUNT(*) AS 'Numero empleados'
+FROM empleados
+GROUP BY codDepto, sexemp;
+
+-- 30. Hallar el salario promedio por departamento
+SELECT codDepto, AVG(salemp) AS 'Salario promedio'
+FROM empleados
+GROUP BY codDepto;
+
+-- 31. Mostrar la lista de los empleados cuyo salario es mayor o igual que el promedio de la empresa. Ordenarlo por departamento
+SELECT nomemp, salemp
+FROM empleados
+WHERE salemp >= (SELECT AVG(SALEMP) FROM empleados);
+
+-- 32. Hallar los departamentos que tienen más de tres empleados. Mostrar el número de empleados de esos departamentos
+SELECT d.codDepto, d.nombreDpto, COUNT(*) AS 'Numero empleados'
+FROM empleados e, departamentos d
+WHERE d.codDepto = e.codDepto
+GROUP BY d.codDepto
+HAVING COUNT(*) >= 3;
+
+-- numero de empleados por departamento
+SELECT codDepto, COUNT() AS 'Numero empleados'
+FROM empleados, coddepto;
--- intro.sql
+++ intro.sql
@@ -18,4 +18,49 @@ INSERT INTO academia.alumnos (dni, nombre, apellido1, apellido2, tlf)
 -- Si se van a agregar valores para todas las columnas de una tabla, no es necesario indicar las columnas:
 INSERT INTO academia.alumnos
 	VALUES ('45678923B', 'Armando', 'Casas', 'Rodríguez', 963852741, 'Portugalete', 42521);
-   
+    
+-- SELECT DISTINCT
+
+
+
+-- MIN() Y MAX()
+-- Funciones que recogen el mínimo y el máximo valor de una columna, respectivamente
+SELECT MIN(population) FROM world.city;
+SELECT MAX(population) FROM world.city;
+
+-- COUNT(), AVG(), SUM()
+-- COUNT() retorna el número de registros que coinciden con la query
+SELECT COUNT(*) FROM world.city
+WHERE population > 1500000;
+
+
+-- AVG() retorna el promedio de los valores de una columna
+SELECT AVG(city.population) 'media de habitantes de las ciudades de España' FROM world.city
+JOIN world.country ON country.code = city.countryCode
+WHERE country.name = 'spain';
+
+-- SUM() retorna la suma de los valores de una columna
+SELECT SUM(city.population) 'total de habitantes de las ciudades de España, de la base de datos' FROM world.city
+JOIN world.country ON country.code = city.countryCode
+WHERE country.name = 'spain';
+
+-- IN y NOT IN
+-- Se usa para indicar varias opciones 
+SELECT language, name FROM world.country c
+JOIN world.countrylanguage l ON c.code = l.countrycode 
+WHERE name IN ('germany', 'france', 'belgium');
+
+-- BETWEEN
+-- Se usa para buscar resultados que estén contenidos entre dos valores
+SELECT name, population FROM world.city
+WHERE population BETWEEN 250000 AND 500000
+OR population BETWEEN 750000 AND 850000;
+
+-- ALIAS 
+SELECT CONCAT(nombre , ' ' , apellido1) AS 'nombre completo' FROM academia.alumnos;
+
+-- GROUP BY
+SELECT COUNT(city.name), country.name FROM world.city
+JOIN world.country ON city.countrycode = country.code
+WHERE continent = 'africa'
+GROUP BY country.name;
